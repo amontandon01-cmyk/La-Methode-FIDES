f24ae527279c56755953dd9454d18bfd6842017b
Mélarthur <amontandon01@gmail.com>
2026-03-04 13:36:35 +0100

    Add navigation data structure in nav.js

---
+++ assets/js/data/nav.js
@@ -0,0 +1,38 @@
+// assets/js/data/nav.js
+export const NAV = [
+  { section: "Vue d’ensemble", items: ["home"] },
+
+  { section: "Socles", items: ["socle_confort", "socle_blessures", "socle_empathie"] },
+
+  { section: "Blessures", items: ["w_rejet", "w_abandon", "w_humiliation", "w_trahison", "w_injustice"] },
+
+  {
+    section: "Sentiments (référentiels)",
+    items: [
+      "ref_sentiments_expansifs",
+      "ref_peur_insecurite",
+      "ref_colere_frustration",
+      "ref_tristesse_perte",
+      "ref_injustice_humiliation",
+      "ref_ouverture_connexion"
+    ]
+  },
+
+  {
+    section: "Addictions / Valeurs / Dogmes",
+    items: [
+      "ref_addictions",
+      "ref_add_substances",
+      "ref_add_comportementales",
+      "ref_add_invisibles",
+      "ref_valeurs",
+      "ref_valeurs_listes",
+      "ref_dogmes",
+      "ref_dogmes_listes"
+    ]
+  },
+
+  { section: "Empathie (impacts)", items: ["impact_apathie", "impact_empathie"] },
+
+  { section: "Référence", items: ["ref_sources"] }
+];
