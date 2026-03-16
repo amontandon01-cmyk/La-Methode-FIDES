527626fe5f2846d0fba2f793d8957f7e119304c5
Mélarthur <amontandon01@gmail.com>
2026-03-16 16:34:34 +0100

    Update data.js

--- data.js
+++ data.js
@@ -503,8 +503,7 @@ const PAGES = [
       ["Croyances limitantes", "« Je finis toujours seul. » « Personne ne reste jamais avec moi. »"],
       ["Croyances expansives", "« Je suis complet même dans la solitude. » « Je peux créer des liens stables et nourrissants. » « Plus besoin de béquille (addiction) pour accepter la réalité. »",
       ["Guérison", "Quand la blessure d’abandon s’apaise, la personne se sent mieux lorsqu’elle est seule, cherche moins l’attention, vit les épreuves avec moins de dramatisation et ose avancer sans attendre l’aval des autres."],
-      ],
-    ],
+   ],
   },
 
   {
